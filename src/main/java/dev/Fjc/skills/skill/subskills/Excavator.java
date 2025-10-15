651c8dda9cf6348ff33c73559c53f6e6f2a8f049
Fjc2032 <charlesfrancoos@gmail.com>
2025-10-14 20:54:20 -0400

    Add more abilities to mining class

--- src/main/java/dev/Fjc/skills/skill/subskills/Excavator.java
+++ src/main/java/dev/Fjc/skills/skill/subskills/Excavator.java
@@ -2,13 +2,23 @@ package dev.Fjc.skills.skill.subskills;
 
 import dev.Fjc.skills.Skills;
 import dev.Fjc.skills.skill.Mining;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
+/**
+ * Represents an Excavator skill, under the main Mining skill
+ * <p>
+ * Abilities: <br>
+ * Vein Miner - Destroy a vein of ores/selected block <br>
+ * Blitz - Blast a large line streak in one direction (initial 3x1 radius)
+ */
 public class Excavator extends Mining {
 
 
@@ -18,8 +28,15 @@ public class Excavator extends Mining {
 
     public void vein(BlockBreakEvent event) {
         if (canGetXP(event)) {
-            for (Block block : veinBlocks(event.getBlock())) block.breakNaturally();
+            for (Block block : veinBlocks(event.getBlock())) {
+                if (!pickaxes.contains(event.getPlayer().getInventory().getItemInMainHand().getType())) continue;
+                block.breakNaturally();
+            }
         }
+    }
+
+    public void blitz(BlockBreakEvent event) {
+        for (Block block : blitzBlocks(event.getBlock())) block.breakNaturally();
     }
 
     private List<Block> veinBlocks(Block block) {
@@ -32,5 +49,32 @@ public class Excavator extends Mining {
         }
 
         return objects;
+    }
+
+    private List<Block> blitzBlocks(Block block) {
+        World world = block.getWorld();
+        double yPrime = block.getLocation().getY() - 1;
+        double yDoublePrime = block.getLocation().getY() + 1;
+
+        List<Block> objects = new ArrayList<>(Byte.MAX_VALUE);
+
+        Location first = new Location(world, block.getX(), yPrime, block.getZ());
+        Block block1 = first.getBlock();
+        objects.add(block1);
+
+        Location second = new Location(world, block.getX(), yDoublePrime, block.getZ());
+        Block block2 = second.getBlock();
+        objects.add(block2);
+
+        for (int i = 0; i < 24; i++) {
+            Block block3 = new Location(world, block.getX() + i, yPrime, block.getZ()).getBlock();
+            objects.add(block3);
+
+            Block block4 = new Location(world, block.getX(), yDoublePrime, block.getZ() + i).getBlock();
+            objects.add(block4);
+        }
+        return objects.stream()
+                .filter(Objects::nonNull)
+                .toList();
     }
 }
